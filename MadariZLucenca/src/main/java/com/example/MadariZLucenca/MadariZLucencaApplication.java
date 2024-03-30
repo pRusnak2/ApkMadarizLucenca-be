fb58d5623cfa2d6f3361d50e742edc6bbca6cd6b
pRusnak2 <prusnak2@student.umb.sk>
2024-03-30 19:34:54 +0100

    zaklad

---
+++ MadariZLucenca/src/main/java/com/example/MadariZLucenca/MadariZLucencaApplication.java
@@ -0,0 +1,13 @@
+package com.example.MadariZLucenca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadariZLucencaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadariZLucencaApplication.class, args);
+	}
+
+}
---
+++ MadariZLucenca/src/test/java/com/example/MadariZLucenca/MadariZLucencaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MadariZLucenca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadariZLucencaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
