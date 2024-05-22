e66321b27387b6b5f11835f4fcd8242fb509fb37
pRusnak2 <prusnak2@student.umb.sk>
2024-05-22 21:11:11 +0200

    test

--- MadariZLucenca/src/main/java/com/example/MadariZLucenca/persistence/TokenEntity.java
+++ MadariZLucenca/src/main/java/com/example/MadariZLucenca/persistence/TokenEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class TokenEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
