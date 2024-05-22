93bebd23a6610aa2afd7bbe567044550e978f833
pRusnak2 <prusnak2@student.umb.sk>
2024-05-22 20:56:11 +0200

    test

--- MadariZLucenca/src/main/java/com/example/MadariZLucenca/persistence/RoleEntity.java
+++ MadariZLucenca/src/main/java/com/example/MadariZLucenca/persistence/RoleEntity.java
@@ -16,7 +16,7 @@ import java.util.Collection;
 @AllArgsConstructor
 public class RoleEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
